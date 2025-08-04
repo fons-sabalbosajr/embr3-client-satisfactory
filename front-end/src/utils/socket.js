51a8d7af9f6dc858ef651eec55cb45f7af3cacc5
Fons Sabalbosa <86940332+fons-sabalbosajr@users.noreply.github.com>
2025-08-04 10:17:02 +0800

    update api and cors issue

--- front-end/src/utils/socket.js
+++ front-end/src/utils/socket.js
@@ -1,12 +1,15 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  transports: ["websocket", "polling"],
+  transports: ["websocket"],
+  withCredentials: true,
   reconnectionAttempts: 5,
-  
 });
 
-console.log("Socket connecting to:", import.meta.env.VITE_SOCKET_URL);
+socket.on("connect_error", (err) => {
+  console.error("❌ Socket connection error:", err.message);
+});
+
+//console.log("Socket connecting to:", import.meta.env.VITE_SOCKET_URL);
 
 export default socket;
